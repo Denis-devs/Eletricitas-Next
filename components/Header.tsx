31a0b678897de2bad5ccedfc02d390db6019d978
Denis-devs <denis.lemos.rodrigues@gmail.com>
2026-06-24 21:18:08 -0300

    Change header to section in Header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <header className="flex justify-center items-center px-8 py-6 bg-gradient-to-b from-black/80 to-transparent">
+    <section className="flex justify-center items-center px-8 py-6 bg-gradient-to-b from-black/80 to-transparent">
       <div className="flex flex-col items-center text-yellow-500 font-bold">
         <Image src="/logo.png" alt="Logo" width={80} height={80} />
         <span className="text-6xl">Rodrigues Eletricista</span>
@@ -11,6 +11,6 @@ export default function Header() {
     Serviços elétricos residenciais, instalação de câmeras, atendimento rápido e profissional.
   </p>
       </div>
-    </header>
+    </section>
   );
 }
