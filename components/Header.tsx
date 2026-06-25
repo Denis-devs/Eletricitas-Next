2352cf47d7004ca71e6feed45b0e3dac889d015d
Denis-devs <denis.lemos.rodrigues@gmail.com>
2026-06-25 08:03:52 -0300

    Replace spans with headings in Header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,8 +5,8 @@ export default function Header() {
     <section className="flex justify-center items-center px-8 py-6 bg-gradient-to-b from-black/80 to-transparent">
       <div className="flex flex-col items-center text-yellow-500 font-bold">
         <Image src="/logo.png" alt="Logo" width={80} height={80} />
-        <span className="text-6xl">Rodrigues Eletricista</span>
-        <span className="text-4xl">Segurança e Qualidade</span>
+        <h1 className="text-6xl">Rodrigues Eletricista</h1>
+        <h2 className="text-4xl">Segurança e Qualidade</h2>
         <p className="text-lg mt-4 text-white text-center max-w-4xl">
     Serviços elétricos residenciais, instalação de câmeras, atendimento rápido e profissional.
   </p>
